af796c9ffe91078b83b9fdfe7726a6ff020e5d5e
ShailJoshi007 <shailbala.joshi@gmail.com>
2023-07-12 11:12:25 +0530

    CommitforJenkis

---
+++ projectTest/src/test/java/projectTestPack/browser.java
@@ -0,0 +1,12 @@
+package projectTestPack;
+
+import org.testng.annotations.Test;
+
+public class browser {
+@Test
+	public void mi()
+	{
+		System.out.println("Hello from Local to gitr to Jenkins");
+	}
+
+}
